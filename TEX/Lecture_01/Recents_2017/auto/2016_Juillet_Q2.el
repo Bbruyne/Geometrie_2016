1ac04b39e3985a4beb9892c4acf4fc4ae1f23c69
Bbruyne <benjamin.debruyne3@gmail.com>
2017-07-10 15:58:32 +0200

    recents + théorie synthetique

---
+++ TEX/Lecture_01/Recents_2017/auto/2016_Juillet_Q2.el
@@ -0,0 +1,33 @@
+(TeX-add-style-hook
+ "2016_Juillet_Q2"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "10pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("babel" "frenchb") ("inputenc" "utf8")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "beamer"
+    "beamer10"
+    "babel"
+    "inputenc"
+    "pgfpages"
+    "dirtree"
+    "tcolorbox"
+    "tikz"
+    "amsmath"
+    "graphicx")
+   (TeX-add-symbols
+    '("notedir" 1)
+    "enonce"
+    "procede"))
+ :latex)
+
